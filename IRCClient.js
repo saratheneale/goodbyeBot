6152dc1bebf8060509379f46b9e0c8b72a6e6267
saratheneale <saratheneale@gmail.com>
2013-03-12 19:07:49 -0700

    Refactored code to make an IRC Client class
    I have a lot of ideas for IRC bots, so I made the IRCClient.js file to
    make these things easier. I've applied it to the very simple Goodbye Bot
    here.

--- IRCClient.js
+++ IRCClient.js
@@ -1,4 +1,3 @@
-//Goodbye Bot pseudocode
 //create IRCClient class/object
 function IRCClient(){
   this.socketId;
@@ -302,60 +301,4 @@ IRCClient.prototype.getRandomGoodVibe =function(user){
         }
         return strMsg;
      }
-}
-function main(){
-//connect to server
-console.log("We're in main")
-bot= new IRCClient();
-bot.setUserName("SaraGoodbyeBot");
-bot.connectToServer(6667,"10.0.0.30");
-bot.goodVibes = ["Goodbye.", "Goodbye, $user"];
-
-//TODO: arg is one of the messages in message.arg array. we only have one spot for msg sender. however, message.messageSender will be populated with whoever sent the arg message.
-//		There's probably a cleaner way to do this. Rethink the message object. 
-
-bot.onMessage= function(message, arg){
-	//do privmsg special handling here.
-	console.log("Calling OnMessage"+message)
-//if message is sent to the channel:
-if(message.username === this.channelName)
-  {
-	//if privmsg channel, and you see hello, say goodbye, leave, and rejoin in 10 seconds
-	if(arg.search(/hello/i) != -1) 
-      {
-        
-        
-      //  console.log(message.msgSender+" that was the message.msgSender");
-        //grab a random thing to say
-        var strRandomGoodVibe = this.getRandomGoodVibe(message.msgSender); 
-        this.write("PRIVMSG " + this.channelName + " :"+strRandomGoodVibe);
-        //leave channel.
-        this.write("PART "+ bot.channelName);
-        //need to join again in 10 seconds.
-        setTimeout(function(){bot.write('JOIN ' + bot.channelName);},10000);
-      }
-  }
-//otherwise, its a message directly to me
-else
-	{
-		var messagingUser = message.prefix.slice(1, message.prefix.search("!"));
-		write("PRIVMSG " + messagingUser + " :Goodbye");
-	}
-
-}
-bot.joinWhenConnected = function(){
-	//function called when join is ready.
-	if(bot.connected)
-		bot.join("#realtestchannel")
-}//end joinWhenConnected
-bot.pingResp = function(){
-	if(bot.connected)
-		this.pong();
-}
-}//end main
-if(window.chrome.socket)
-{
-  debugger;
-  main();
-
-}
+}//end getRandomGoodVibe
